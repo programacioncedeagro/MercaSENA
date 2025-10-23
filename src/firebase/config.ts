68f02eaa3a0bb4bc8f22999958fbe13816a1d48a
H DIM <harveydimate@gmail.com>
2025-10-23 16:08:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5379754521-69f86",
+  "appId": "1:943518391543:web:3fbea07ee3b500a56b87f2",
+  "apiKey": "AIzaSyBMtF7Nj5tMwKVPyhe4zKYxJodKbqDhAIo",
+  "authDomain": "studio-5379754521-69f86.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "943518391543"
+};
