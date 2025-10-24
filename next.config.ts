a6cdbacc32fb483a1cac5ecc4d0c25258f3ada1e
H DIM <harveydimate@gmail.com>
2025-10-24 16:03:00 +0000

    Creating an optimized production build ...  ✓ Compiled successfully in 2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['tsx', 'ts'],
+  experimental: {
+    appDir: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
